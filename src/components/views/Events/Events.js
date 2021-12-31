9685be89292d0efcefe29fe7761ad431cdcd800d
Maciek Mielczarek <maciejka-67@o2.pl>
2021-12-31 12:07:37 +0100

    Add Events and EventsNew components

--- src/components/views/Events/Events.js
+++ src/components/views/Events/Events.js
@@ -1,13 +1,237 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { MuiPickersUtilsProvider, TimePicker } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import Slider from '@material-ui/core/Slider';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
 import PropTypes from 'prop-types';
 import styles from './Events.module.scss';
 
-const Events = ({match}) => (
-  <div className={styles.component}>
-    <h2>Events View</h2>
-    <h3>{`Event id: ${match.params.id}`}</h3>
-  </div>
-);
+const event = {
+  date: '2021-12-31',
+  hour: '22:00',
+  table: 1,
+  duration: 4,
+  id: 3,
+  repeat: 'daily',
+  starters: [
+    'water',
+    'bread',
+  ],
+  ppl: 4,
+  phone: '666666666',
+  address: 'London Zdroj 6',
+};
+
+const stepsNumber = 5;
+
+const generateMarks = stepsNumber => {
+  const marks = [
+    {
+      value: 1,
+      label: '1 hour',
+    },
+  ];
+  for(let i=2; i<=stepsNumber; i++){
+    marks.push(
+      {
+        value: i,
+        label: i + 'hours',
+      }
+    );
+  }
+  return marks;
+};
+
+const Events = ({match}) => {
+  const [isDisabled, setActive] = useState(true);
+  const [bookingTime, setBookingTime] = useState(new Date(event.date + 'T' + event.hour));
+  const [sliderValue, setSliderValue] = useState(event.duration);
+  const handleSliderValueChange = (event, newValue) => {
+    setSliderValue(newValue);
+  };
+  const [selectedTable, setSelectedTable] = useState(event.table);
+  const handleTableChange = (event) => {
+    setSelectedTable(event.target.value * 1);
+  };
+  const [people, setPeople] = useState(event.ppl);
+  const handlePeopleChange = (event) => {
+    setPeople(event.target.value);
+  };
+  const [starters, setStarters] = useState({
+    water: event.starters.indexOf('water') !== -1,
+    cola: event.starters.indexOf('cola') !== -1,
+    bread: event.starters.indexOf('bread') !== -1,
+  });
+  const handleStartersChange = (event) => {
+    setStarters({ ...starters, [event.target.name]: event.target.checked });
+  };
+  const [address, setAddress] = useState(event.address);
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+  };
+  const [phone, setPhone] = useState(event.phone);
+  const handlePhoneChange = (event) => {
+    setPhone(event.target.value);
+  };
+  return (
+    <Paper className={styles.component}>
+      <h2 className={styles.header}>
+        {`Event of id ${match.params.id} details`}
+      </h2>
+      <div className={styles.eventInfo}>
+        <Typography gutterBottom>
+          {`Event frequency: ${event.repeat}.`}
+        </Typography>
+        <Typography gutterBottom>
+          {`First event date: ${new Date(event.date).toDateString()}.`}
+        </Typography>
+      </div>
+      <Grid container spacing={3}>
+        <Grid item className={styles.gridItem} xs={12} md={6}>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <TimePicker
+              inputVariant='outlined'
+              label='Event time start'
+              value={bookingTime}
+              onChange={setBookingTime}
+              disabled={isDisabled ? true : false}
+            />
+          </MuiPickersUtilsProvider>
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={6}>
+          <div className={styles.slider}>
+            <Typography gutterBottom>
+              Event duration
+            </Typography>
+            <Slider
+              value={sliderValue}
+              onChange={handleSliderValueChange}
+              valueLabelDisplay="auto"
+              step={1}
+              marks={generateMarks(stepsNumber)}
+              min={1}
+              max={stepsNumber}
+              color="secondary"
+              disabled={isDisabled ? true : false}
+            />
+          </div>
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <FormControl component="fieldset">
+            <Typography gutterBottom>
+              Table
+            </Typography>
+            <RadioGroup name="table" value={selectedTable} onChange={handleTableChange}>
+              <FormControlLabel
+                value={1}
+                control={<Radio />}
+                label="table 1"
+                disabled={isDisabled ? true : false}
+              />
+              <FormControlLabel
+                value={2}
+                control={<Radio />}
+                label="table 2"
+                disabled={isDisabled ? true : false}
+              />
+              <FormControlLabel
+                value={3}
+                control={<Radio />}
+                label="table 3"
+                disabled={isDisabled ? true : false}
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <TextField
+            label="People"
+            type="number"
+            value={people}
+            onChange={handlePeopleChange}
+            variant="outlined"
+            disabled={isDisabled ? true : false}
+          />
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <FormControl component="fieldset">
+            <Typography gutterBottom>
+              Starters
+            </Typography>
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox
+                  checked={starters.water}
+                  onChange={handleStartersChange}
+                  name="water"
+                />}
+                label="Water"
+                disabled={isDisabled ? true : false}
+              />
+              <FormControlLabel
+                control={<Checkbox
+                  checked={starters.cola}
+                  onChange={handleStartersChange}
+                  name="cola"
+                />}
+                label="Cola"
+                disabled={isDisabled ? true : false}
+              />
+              <FormControlLabel
+                control={<Checkbox
+                  checked={starters.bread}
+                  onChange={handleStartersChange}
+                  name="bread"
+                />}
+                label="Bread"
+                disabled={isDisabled ? true : false}
+              />
+            </FormGroup>
+          </FormControl>
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <TextField
+            label="Address"
+            value={address}
+            onChange={handleAddressChange}
+            variant="outlined"
+            disabled={isDisabled ? true : false}
+          />
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <TextField
+            label="Phone"
+            value={phone}
+            onChange={handlePhoneChange}
+            variant="outlined"
+            disabled={isDisabled ? true : false}
+          />
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={()=>setActive(!isDisabled)}
+          >
+            {isDisabled ? 'Edit' : 'Save'}
+          </Button>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
 
 Events.propTypes = {
   match: PropTypes.object,
--- src/components/views/EventsNew/EventsNew.js
+++ src/components/views/EventsNew/EventsNew.js
@@ -1,10 +1,174 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { MuiPickersUtilsProvider, DatePicker, TimePicker } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import Slider from '@material-ui/core/Slider';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
 import styles from './EventsNew.module.scss';
 
-const EventsNew = () => (
-  <div className={styles.component}>
-    <h2>EventsNew View</h2>
-  </div>
-);
+const stepsNumber = 5;
+
+const generateMarks = stepsNumber => {
+  const marks = [
+    {
+      value: 1,
+      label: '1 hour',
+    },
+  ];
+  for(let i=2; i<=stepsNumber; i++){
+    marks.push(
+      {
+        value: i,
+        label: i + 'hours',
+      }
+    );
+  }
+  return marks;
+};
+
+const EventsNew = () => {
+  const timeNow = new Date();
+  const [bookingDate, setBookingDate] = useState(timeNow);
+  const [bookingTime, setBookingTime] = useState(timeNow);
+  const [selectedTable, setSelectedTable] = useState(1);
+  const handleTableChange = (event) => {
+    setSelectedTable(event.target.value * 1);
+  };
+  return (
+    <Paper className={styles.component}>
+      <h2 className={styles.header}>
+        Start new event
+      </h2>
+      <Grid container spacing={3}>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <DatePicker
+              inputVariant='outlined'
+              label='Event first date'
+              value={bookingDate}
+              onChange={setBookingDate}
+              disablePast
+            />
+          </MuiPickersUtilsProvider>
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <TimePicker
+              inputVariant='outlined'
+              label='Event start time'
+              value={bookingTime}
+              onChange={setBookingTime}
+            />
+          </MuiPickersUtilsProvider>
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <div className={styles.slider}>
+            <Typography gutterBottom>
+              Event duration
+            </Typography>
+            <Slider
+              defaultValue={1}
+              valueLabelDisplay="auto"
+              step={1}
+              marks={generateMarks(stepsNumber)}
+              min={1}
+              max={stepsNumber}
+              color="secondary"
+            />
+          </div>
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <FormControl component="fieldset">
+            <Typography gutterBottom>
+              Table
+            </Typography>
+            <RadioGroup name="table" value={selectedTable} onChange={handleTableChange}>
+              <FormControlLabel
+                value={1}
+                control={<Radio />}
+                label="table 1"
+              />
+              <FormControlLabel
+                value={2}
+                control={<Radio />}
+                label="table 2"
+              />
+              <FormControlLabel
+                value={3}
+                control={<Radio />}
+                label="table 3"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <TextField
+            label="People"
+            type="number"
+            defaultValue={1}
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <FormControl component="fieldset">
+            <Typography gutterBottom>
+              Starters
+            </Typography>
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox
+                  name="water"
+                />}
+                label="Water"
+              />
+              <FormControlLabel
+                control={<Checkbox
+                  name="cola"
+                />}
+                label="Cola"
+              />
+              <FormControlLabel
+                control={<Checkbox
+                  name="bread"
+                />}
+                label="Bread"
+              />
+            </FormGroup>
+          </FormControl>
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <TextField
+            label="Address"
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <TextField
+            label="Phone"
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item className={styles.gridItem} xs={12} md={4}>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+          >
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
 
 export default EventsNew;
